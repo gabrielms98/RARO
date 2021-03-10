ee6b534399617cf6233339c32cc5b96523daa20b
Gabriel Martins <gmbiel1@gmail.com>
2021-03-10 18:04:27 -0300

    :rocket: Project Init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <LandingPage />
+</template>
+
+<script>
+import LandingPage from './views/LandingPage.vue';
+
+export default {
+  components: {
+    LandingPage,
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss">
+@import "./scss/styles";
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Poppins, sans-serif;
+}
+
+body {
+  background: $white;
+}
+</style>
